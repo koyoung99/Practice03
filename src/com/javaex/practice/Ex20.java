48aa18e32497aba88eb3c5276689c2d80d625262
김아영 <72481734+koyoung99@users.noreply.github.com>
2024-01-02 17:04:49 +0900

    Ex20 수정

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -8,35 +8,45 @@ public class Ex20 {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("================================");
-		System.out.println("\t[숫자맞추기게임 시작]");
-		System.out.println("================================");
-
-		int num = (int) (Math.random() * 100) + 1;
-		System.out.println(num);
-
-		int result = 0;
-
 		while (true) {
 
-			System.out.print(">> ");
-			result = sc.nextInt();
+			int num = (int) (Math.random() * 100) + 1;
+			int result;
 
-			if (num > result) {
-				System.out.println("더높게");
-			} else if (num < result) {
-				System.out.println("더낮게");
-			} else {
-				System.out.println("맞았습니다.");
-				System.out.print("게임을 종료하시겠습니까?(y/n)");
-				String ans = sc.nextLine();
-				if (ans.equals("y")) {
-					System.out.println("================================");
-					System.out.println("\t[숫자맞추기게임 종료]");
-					System.out.println("================================");
+			System.out.println("========================");
+			System.out.println("    [숫자맞추기게임 시작]");
+			System.out.println("========================");
+
+			while (true) {
+
+				System.out.print(">>");
+				result = sc.nextInt();
+
+				if (result < num) {
+					System.out.println("더 높게");
+				} else if (result > num) {
+					System.out.println("더 낮게");
+				} else {
+					System.out.println("맞았습니다.");
+					break;
 				}
-			}
 
+			}
+			sc.nextLine();
+
+			System.out.print("게임을 종료하시겠습니까?(y/n) >>");
+			String ans = sc.next();
+			if ("y".equals(ans)) {
+				System.out.println("========================");
+				System.out.println("    [숫자맞추기게임 종료]");
+				System.out.println("========================");
+
+				break;
+			}
 		}
+
+		sc.close();
+
 	}
+
 }
